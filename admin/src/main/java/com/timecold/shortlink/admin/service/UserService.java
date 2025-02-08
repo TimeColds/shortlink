17c6f6652d5482d7b940ea868312c168964fb08b
TimeCold <l2878423090@gmail.com>
2025-02-08 14:29:50 +0800

    用户注册功能和用户名重复检测功能

--- admin/src/main/java/com/timecold/shortlink/admin/service/UserService.java
+++ admin/src/main/java/com/timecold/shortlink/admin/service/UserService.java
@@ -2,6 +2,7 @@ package com.timecold.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.timecold.shortlink.admin.dao.entity.UserDO;
+import com.timecold.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.timecold.shortlink.admin.dto.resp.UserRespDTO;
 
 /**
@@ -15,4 +16,18 @@ public interface UserService extends IService<UserDO> {
      * @return 用户返回实体
      */
     UserRespDTO getUserByUsername(String username);
+
+    /**
+     * 查询用户名是否存在
+     * @param username 用户名
+     * @return 用户存在返回 True, 不存在返回 False
+     */
+    Boolean hasUserName(String username);
+
+    /**
+     * 注册用户
+     *
+     * @param requestParam 注册用户请求参数
+     */
+    void register(UserRegisterReqDTO requestParam);
 }
--- admin/src/main/java/com/timecold/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/timecold/shortlink/admin/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.timecold.shortlink.admin.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -7,8 +8,11 @@ import com.timecold.shortlink.admin.common.convention.exception.ClientException;
 import com.timecold.shortlink.admin.common.enums.UserErrorCodeEnum;
 import com.timecold.shortlink.admin.dao.entity.UserDO;
 import com.timecold.shortlink.admin.dao.mapper.UserMapper;
+import com.timecold.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.timecold.shortlink.admin.dto.resp.UserRespDTO;
 import com.timecold.shortlink.admin.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RBloomFilter;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +20,10 @@ import org.springframework.stereotype.Service;
  * 用户接口实现层
  */
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
+
+    private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
 
     @Override
     public UserRespDTO getUserByUsername(String username) {
@@ -29,5 +36,22 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         UserRespDTO result = new UserRespDTO();
         BeanUtils.copyProperties(userDO, result);
         return result;
+    }
+
+    @Override
+    public Boolean hasUserName(String username) {
+        return !userRegisterCachePenetrationBloomFilter.contains(username);
+    }
+
+    @Override
+    public void register(UserRegisterReqDTO requestParam) {
+        if (!hasUserName(requestParam.getUsername())) {
+            throw new ClientException(UserErrorCodeEnum.USER_EXIST);
+        }
+        int insert = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+        if (insert < 1) {
+            throw new ClientException(UserErrorCodeEnum.USER_SAVE_ERROR);
+        }
+        userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
     }
 }
