49e522dd11b128d79626612262e0151e26c82e2e
TimeCold <l2878423090@gmail.com>
2025-02-09 13:47:09 +0800

    开发数据库持久层对象基础属性

---
+++ admin/src/main/java/com/timecold/shortlink/admin/common/database/BaseDO.java
@@ -0,0 +1,29 @@
+package com.timecold.shortlink.admin.common.database;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 数据库持久层对象基础属性
+ */
+@Data
+public class BaseDO {
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+    /**
+     * 删除表示 0:未删除 1:已删除
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Integer delFlag;
+}
