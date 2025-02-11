14aa9b657a4806cfd9d29c0fa08f9fff9a13935d
TimeCold <l2878423090@gmail.com>
2025-02-11 11:11:59 +0800

    修复命名错误

--- admin/src/main/java/com/timecold/shortlink/admin/config/RBloomFilterConfiguration.java
+++ admin/src/main/java/com/timecold/shortlink/admin/config/RBloomFilterConfiguration.java
@@ -13,7 +13,7 @@ public class RBloomFilterConfiguration {
      * 防止用户注册查询数据库的布隆过滤器
      */
     @Bean
-    public RBloomFilter<String> shortURIRegisterCachePenetrationBloomFilter(RedissonClient redissonClient) {
+    public RBloomFilter<String> userRegisterCachePenetrationBloomFilter(RedissonClient redissonClient) {
         RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter("userRegisterCachePenetrationBloomFilter");
         cachePenetrationBloomFilter.tryInit(100000000L, 0.001);
         return cachePenetrationBloomFilter;
