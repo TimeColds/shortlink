17a931eb0b310f7e31b69cd21f0dfbc2e00a3dc6
TimeCold <l2878423090@gmail.com>
2025-02-19 16:19:37 +0800

    引入Apache HttpClient5 优化RestTemplate性能

--- project/src/main/java/com/timecold/shortlink/project/config/RestTemplateConfig.java
+++ project/src/main/java/com/timecold/shortlink/project/config/RestTemplateConfig.java
@@ -1,4 +1,4 @@
-package com.timecold.shortlink.admin.config;
+package com.timecold.shortlink.project.config;
 
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
@@ -15,7 +15,6 @@ import java.util.List;
 
 @Configuration
 public class RestTemplateConfig {
-
 
     @Bean
     public RestTemplate restTemplate() {
