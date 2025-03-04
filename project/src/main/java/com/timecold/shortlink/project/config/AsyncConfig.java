59a59f8340caa6268d32dbbb5ff31b6669709468
TimeCold <l2878423090@gmail.com>
2025-03-04 22:49:20 +0800

    配置自定义线程池

---
+++ project/src/main/java/com/timecold/shortlink/project/config/AsyncConfig.java
@@ -0,0 +1,53 @@
+package com.timecold.shortlink.project.config;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.Arrays;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
+@EnableAsync
+@Slf4j
+public class AsyncConfig {
+
+    @Bean("statsThreadPool")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        // 根据服务器CPU核心数配置
+        int corePoolSize = Runtime.getRuntime().availableProcessors() * 2;
+        //配置核心线程数
+        executor.setCorePoolSize(corePoolSize);
+        //配置最大线程数
+        executor.setMaxPoolSize(corePoolSize * 2);
+        //配置队列大小
+        executor.setQueueCapacity(200);
+        //线程池维护线程所允许的空闲时间
+        executor.setKeepAliveSeconds(30);
+        //配置线程池中的线程的名称前缀
+        executor.setThreadNamePrefix("short-link-stats-");
+        //设置线程池关闭的时候等待所有任务都完成再继续销毁其他的Bean
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        //设置线程池中任务的等待时间，如果超过这个时候还没有销毁就强制销毁，以确保应用最后能够被关闭，而不是阻塞住
+        executor.setAwaitTerminationSeconds(60);
+        // rejection-policy：当pool已经达到max size的时候，如何处理新任务
+        // CALLER_RUNS：不在新线程中执行任务，而是由调用者所在的线程来执行
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        //执行初始化
+        executor.initialize();
+        return executor;
+    }
+
+    @Bean
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return (ex, method, params) -> {
+            log.error("异步任务执行出错, 方法: {}, 参数: {}", method.getName(), Arrays.toString(params), ex);
+        };
+    }
+}
