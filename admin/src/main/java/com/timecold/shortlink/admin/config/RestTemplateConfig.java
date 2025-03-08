add803dae6c70ea0dc4b450b3c915b203b97e6b6
TimeCold <l2878423090@gmail.com>
2025-03-08 21:47:37 +0800

    修改admin中jackson对LocalDate和LocalDateTime的序列化

--- admin/src/main/java/com/timecold/shortlink/admin/config/RestTemplateConfig.java
+++ admin/src/main/java/com/timecold/shortlink/admin/config/RestTemplateConfig.java
@@ -1,5 +1,7 @@
 package com.timecold.shortlink.admin.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
@@ -8,14 +10,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class RestTemplateConfig {
 
+    private final ObjectMapper objectMapper;
 
     @Bean
     public RestTemplate restTemplate() {
@@ -31,6 +36,9 @@ public class RestTemplateConfig {
         for (HttpMessageConverter<?> messageConverter : messageConverters) {
             if (messageConverter instanceof StringHttpMessageConverter) {
                 ((StringHttpMessageConverter) messageConverter).setDefaultCharset(StandardCharsets.UTF_8);
+            }
+            if (messageConverter instanceof MappingJackson2HttpMessageConverter) {
+                ((MappingJackson2HttpMessageConverter) messageConverter).setObjectMapper(objectMapper);
             }
         }
         return restTemplate;
