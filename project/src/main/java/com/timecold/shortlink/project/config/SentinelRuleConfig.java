47c92cefce37d53e994b69b47821f2d8e0d17757
TimeCold <l2878423090@gmail.com>
2025-03-11 09:45:24 +0800

    分页查询短链接添加流控

--- project/src/main/java/com/timecold/shortlink/project/config/SentinelRuleConfig.java
+++ project/src/main/java/com/timecold/shortlink/project/config/SentinelRuleConfig.java
@@ -14,12 +14,21 @@ public class SentinelRuleConfig implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() throws Exception {
+        // 创建短链接的限流规则
         List<FlowRule> rules = new ArrayList<>();
         FlowRule createOrderRule = new FlowRule();
         createOrderRule.setResource("create_short-link");
         createOrderRule.setGrade(RuleConstant.FLOW_GRADE_QPS);
-        createOrderRule.setCount(1);
+        createOrderRule.setCount(10);
         rules.add(createOrderRule);
+
+        // 分页查询的限流规则
+        FlowRule pageShortLinkRule = new FlowRule();
+        pageShortLinkRule.setResource("page_short-link");
+        pageShortLinkRule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        pageShortLinkRule.setCount(10);
+        rules.add(pageShortLinkRule);
+
         FlowRuleManager.loadRules(rules);
     }
 }
--- project/src/main/java/com/timecold/shortlink/project/handler/CustomBlockHandler.java
+++ project/src/main/java/com/timecold/shortlink/project/handler/CustomBlockHandler.java
@@ -1,12 +1,19 @@
 package com.timecold.shortlink.project.handler;
 
 import com.alibaba.csp.sentinel.slots.block.BlockException;
-import com.timecold.shortlink.project.common.convention.exception.ServiceException;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.timecold.shortlink.project.common.convention.result.Result;
 import com.timecold.shortlink.project.dto.req.ShortLinkCreateReqDTO;
+import com.timecold.shortlink.project.dto.req.ShortLinkPageReqDTO;
 import com.timecold.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
+import com.timecold.shortlink.project.dto.resp.ShortLinkPageRespDTO;
 
 public class CustomBlockHandler {
-    public static ShortLinkCreateRespDTO createShortLinkBlockHandlerMethod(ShortLinkCreateReqDTO requestParam, BlockException exception) {
-        throw new ServiceException("当前访问网站人数过多，请稍后再试...");
+    public static Result<ShortLinkCreateRespDTO> createShortLinkBlockHandlerMethod(ShortLinkCreateReqDTO requestParam, BlockException exception) {
+        return new Result<ShortLinkCreateRespDTO>().setCode("B100000").setMessage("创建过于频繁，请稍候再试...");
+    }
+
+    public static Result<Page<ShortLinkPageRespDTO>> pageShortLinkBlockHandlerMethod(ShortLinkPageReqDTO requestParam, BlockException exception) {
+        return new Result<Page<ShortLinkPageRespDTO>>().setCode("B100000").setMessage("查询过于频繁，请稍候再试...");
     }
 }
